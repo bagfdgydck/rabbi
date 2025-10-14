974408cc5ed26c764701b36a781504cfb2757957
rabbi0973 <oaa51299@gmail.com>
2025-10-14 18:33:26 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -93,6 +93,7 @@ const emojiAudioMap = {
  "🫡": "https://files.catbox.moe/6jo967.mp3",
  "👹": "https://files.catbox.moe/scsxhj.mp3",
  "🫂": "https://files.catbox.moe/4315xb.mp3",
+ "🍁": "https://files.catbox.moe/texnc2.mp3",
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
