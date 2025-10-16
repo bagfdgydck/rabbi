d7c73866d777b55f2d7d256611d04f7d96cd1d94
gdaitefufvb <aidibata59@gmail.com>
2025-10-16 22:59:04 +0600

    Update leavenoti.js

--- Script/events/leavenoti.js
+++ Script/events/leavenoti.js
@@ -18,17 +18,10 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
   const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম নয় এখানে রাব্বি এর ইসলামিক চ্যাট থাকতে তুই লিভ নিস😡😠🤬 \n\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ ☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎𝐂𝐡𝐚𝐭" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই আবাল😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ ☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎. 𝐂𝐡𝐚𝐭";
-  const path = join(__dirname, "Nazrul", "https://i.imgur.com/ecJGRrC.gif",;
+  const path = join(__dirname, "Nazrul", "leaveGif");
   const gifPath = join(path, `leave1.gif`);
   var msg, formPush
 
   if (existsSync(path)) mkdirSync(path, { recursive: true });
 
   (typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} " : msg = data.customLeave;
-  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
-
-  if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
-  else formPush = { body: msg }
-
-  return api.sendMessage(formPush, threadID);
-                                                                  }
