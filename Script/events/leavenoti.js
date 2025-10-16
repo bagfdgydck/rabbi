9e68481c78798f3cc128431f12a65ca10ac28110
gdaitefufvb <aidibata59@gmail.com>
2025-10-16 21:47:27 +0600

    Update leavenoti.js

--- Script/events/leavenoti.js
+++ Script/events/leavenoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "leave",
   eventType: ["log:unsubscribe"],
   version: "1.0.0",
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "rabbi",
   description: "Thông báo bot hoặc người rời khỏi nhóm",
   dependencies: {
     "fs-extra": "",
@@ -12,32 +12,23 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event, Users, Threads }) {
   if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-
   const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-  const { join } = global.nodemodule["path"];
+  const { join } =  global.nodemodule["path"];
   const { threadID } = event;
-
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-
-  const type = (event.author == event.logMessageData.leftParticipantFbId)
-    ? " তোর সাহস কম না  গ্রুপের এডমিনের পারমিশন ছাড়া তুই লিভ  নিস😡😠🤬 \n✦─────꯭─⃝‌‌☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎.𝐂𝐡𝐚𝐭 𝐁𝐨𝐭────✦"
-    : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই ছাগল😡\nতাই তোমাকে লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪 WELLCOME REMOVE🤧\n✦─────꯭─⃝‌‌☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎.𝐂𝐡𝐚𝐭 𝐁𝐨𝐭────✦";
-
-  const path = join(__dirname, "Shahadat", "leaveGif");
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম নয় এখানে রাব্বি এর ইসলামিক চ্যাট থাকতে তুই লিভ নিস😡😠🤬 \n\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ ☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎𝐂𝐡𝐚𝐭" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই আবাল😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧\n⋆✦⎯⎯⎯⎯⎯⎯⎯⎯✦⋆\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➢ ☞︎︎︎𝐑𝐀𝐁𝐁𝐢⍟𝐕𝐀𝐈☜︎︎. 𝐂𝐡𝐚𝐭";
+  const path = join(__dirname, "Nazrul", "leaveGif");
   const gifPath = join(path, `leave1.gif`);
+  var msg, formPush
 
-  if (!existsSync(path)) mkdirSync(path, { recursive: true });
+  if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-  let msg = (typeof data.customLeave == "undefined")
-    ? "ইস {name} {type} "
-    : data.customLeave;
-
+  (typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} " : msg = data.customLeave;
   msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
-  const formPush = existsSync(gifPath)
-    ? { body: msg, attachment: createReadStream(gifPath) }
-    : { body: msg };
+  if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
+  else formPush = { body: msg }
 
   return api.sendMessage(formPush, threadID);
-};
+                                                                  }
